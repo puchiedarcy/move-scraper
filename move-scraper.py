118d8b49a852202c3799b617942d7f6b80727972
Puchie Darcy <puchiedarcy@puchiedarcy.com>
2015-02-28 17:46:33 -0800

    handle XY/ORAS moveset changes

--- move-scraper.py
+++ move-scraper.py
@@ -1,11 +1,24 @@
 import urllib
 from lxml import html
 
-url = 'http://bulbapedia.bulbagarden.net/wiki/Durant_(Pok%C3%A9mon)'
-response = urllib.urlopen(url)
-data = response.read()
-tree = html.fromstring(data)
+pokemon = ['Mew', 'Skitty', 'Durant', 'Lapras', 'Rhyhorn']
 
-learn_by_level_up = tree.xpath('//span[@id="By_leveling_up"]/../following-sibling::table[1]/tr[2]/td[1]/table[1]/tr/td[2]/a/span/text()')
+def get_moves(name, move_name_index = 2):
+    url = 'http://bulbapedia.bulbagarden.net/wiki/{}_(Pok%C3%A9mon)'.format(name)
+    response = urllib.urlopen(url)
+    data = response.read()
+    tree = html.fromstring(data)
 
-print learn_by_level_up
+    learn_by_level_up = tree.xpath('//span[@id="By_leveling_up"]/../following-sibling::table[1]/tr[2]/td[1]/table[1]/tr/td[{}]/a/span/text()'.format(move_name_index))
+    return learn_by_level_up
+
+
+
+for i in pokemon:
+
+    moves = get_moves(i)
+    if len(moves) == 0:
+        moves = get_moves(i, 3)
+
+    print moves
+~                  
